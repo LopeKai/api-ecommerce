3e8e0783c183bd3a9038612a1a2dd2d4fec3878f
Kaique araujo lopes <kaiquearaujo246@hotmail.com>
2026-09-21 19:58:20 -0300

    feature: adding the logged-in user to the request

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -1,15 +1,28 @@
 import expresss, { NextFunction, Request, Response } from 'express';
 import { UnathorizedError } from '../errors/unauthorized.error';
 import { DecodedIdToken, getAuth } from 'firebase-admin/auth';
+import { UserService } from '../services/user.service';
+import { ForbiddenError } from '../errors/forbidden.error';
 
 export const auth = (app: expresss.Express) => {
     app.use(async (req: Request, res: Response, next: NextFunction) => {
+        if(req.method === "POST" && req.url.startsWith("/auth/login")) {
+           return next();
+        };
+
         const token = req.headers.authorization?.split("Bearer ")[1];
 
         if (token) {
             try {
                 const decodedIdToken: DecodedIdToken = await getAuth().verifyIdToken(token);
-                console.log(decodedIdToken)
+                const user = await new UserService().getById(decodedIdToken.uid);
+
+                if(!user) {
+                    return next(new ForbiddenError());
+                };
+
+                req.user = user;
+
                 return next();
             } catch (error) {
                 next(new UnathorizedError());
